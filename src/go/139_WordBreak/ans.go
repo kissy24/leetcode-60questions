7581b8ad1e196258389a2d334b031b5cb64d1601
kissy24 <kissy105808j@gmail.com>
2025-09-27 12:13:25 +0900

    add

---
+++ src/go/139_WordBreak/ans.go
@@ -0,0 +1,14 @@
+func wordBreak(s string, wordDict []string) bool {
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+	for i := 1; i < len(s)+1; i++ {
+		for j := 0; j < i; j++ {
+			if dp[j] && slices.Contains(wordDict, s[j:i]) {
+				dp[i] = true
+				break
+			}
+		}
+		fmt.Println(dp)
+	}
+	return dp[len(s)]
+}
